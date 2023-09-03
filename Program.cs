14b17679f1a4a7d6179e6bbe2280236b9486683b
PayalBansal-24 <137813552+PayalBansal-24@users.noreply.github.com>
2023-09-03 14:44:26 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_Ass3_Exp8
+{
+    internal class Program
+    {
+        private void PrivateMethod()
+        {
+            Console.WriteLine("Private Method calling....");
+        }
+
+        public void PublicMethod()
+        {
+            Console.WriteLine("Public Method is invoking..");
+            PrivateMethod();
+        }
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.PublicMethod();
+        }
+    }
+}
