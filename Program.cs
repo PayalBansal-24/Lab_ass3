53c5ed9f2d2a1394a8ccd6f3189ea2f877d7b097
PayalBansal-24 <137813552+PayalBansal-24@users.noreply.github.com>
2023-09-03 14:57:52 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_Ass3_Exp9
+{
+    public class Program
+    {
+        protected void ProtetedMethod()
+        {
+            Console.WriteLine("Protected method is calling....");
+        }
+
+    }
+}
---
+++ Program1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lab_Ass3_Exp9;
+
+namespace Lab_Ass3_Exp9_1
+{
+    public class DerivedClass : Program
+    {
+        public void PublicMethod()
+        {
+            Console.WriteLine("Calling the protected method..."); 
+            ProtetedMethod();
+        }
+        
+        static void Main(string[] args)
+        {
+            DerivedClass d = new DerivedClass();
+            d.PublicMethod();
+        }
+    }
+}
