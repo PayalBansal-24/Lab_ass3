28ae16d25ebc57d5d26c5b316aadc538b8183f85
PayalBansal-24 <137813552+PayalBansal-24@users.noreply.github.com>
2023-09-03 14:22:03 +0530

    Add files via upload

---
+++ Program1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Exp6;
+
+namespace Exp6_1
+{
+     public class DerivedClass : Program
+    {
+        public void AccessNames()
+        {
+            Console.WriteLine("Full Name: " + first_name + " " +last_name);
+        }
+  
+    
+        static void Main(string[] args)
+        {
+            DerivedClass d = new DerivedClass();
+            d.AccessNames();
+        }
+    }
+}
