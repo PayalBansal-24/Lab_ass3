5fb7686db758fbc307d69b2935eb51eca9e5f731
PayalBansal-24 <137813552+PayalBansal-24@users.noreply.github.com>
2023-09-03 13:25:59 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exp4
+{
+    class BaseClass
+    {
+        protected int bank_acc;
+
+        protected void SetValue(int bank_acc_no)
+        {
+            bank_acc = bank_acc_no;
+        }
+    }
+
+    class DerivedClass : BaseClass
+    {
+        public void AccessMembers(int bank)
+        {
+            Console.WriteLine("Bank account Number is: " + bank);
+        }
+        static void Main(string[] args)
+        {
+            DerivedClass d = new DerivedClass();
+            Console.WriteLine("Enter your Bank account number:");
+            int bank_acc = Convert.ToInt32(Console.ReadLine());
+            d.SetValue(bank_acc);
+            d.AccessMembers(bank_acc);
+        }
+    }
+}
+
