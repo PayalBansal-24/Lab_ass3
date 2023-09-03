c43faf8c0be7d7f53287378379e9568d8abf5552
PayalBansal-24 <137813552+PayalBansal-24@users.noreply.github.com>
2023-09-03 14:03:31 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exp5
+{
+    public class Assembly1
+    {
+
+        protected internal string Account_holder_name = Console.ReadLine();
+
+     }
+
+}
---
+++ Program1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Exp5;
+
+namespace Exp5_1
+{
+    internal class DerivedClass : Assembly1
+    {
+        public void AccountHolderName()
+        {
+            Console.WriteLine("Account holder name is: " + Account_holder_name);
+        }
+    }
+
+    internal class Program { 
+        static void Main(string[] args)
+        {
+            DerivedClass a = new DerivedClass();
+            a.AccountHolderName();
+        }
+    }
+}
