03ea44e827e9eb23efafe11743e68d7ba19b89ef
PayalBansal-24 <137813552+PayalBansal-24@users.noreply.github.com>
2023-09-03 13:04:14 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exp3
+{
+    internal class Assembly1
+    {
+        internal string sName;
+        public void studName(string name)
+        {
+            sName = name;
+        }
+
+        public string GetName()
+        {
+            return sName;
+        }
+    }
+
+}
