0499a3a5d14078047c4ea8bca732fa1953bf553b
PayalBansal-24 <137813552+PayalBansal-24@users.noreply.github.com>
2023-09-03 15:07:44 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_Ass3_Exp10
+{
+    public class Program
+    {
+        internal Program() {
+            Console.WriteLine("Internal Constructor calling");
+        }
+
+        public void add(int x, int y)
+        {
+            int add = x + y;
+            Console.WriteLine(add);
+        }
+        
+    }
+}
---
+++ Program1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lab_Ass3_Exp10;
+
+namespace Lab_Ass3_Exp10_1
+{
+    public class Program1
+    {
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            Console.Write("Enter first number: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter second number: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Addition: ");
+            p.add(a, b);
+        }
+    }
+}
