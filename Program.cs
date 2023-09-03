19a65dc3acc73459664e9f53a62c0e5ccb365566
PayalBansal-24 <137813552+PayalBansal-24@users.noreply.github.com>
2023-09-03 14:37:19 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_Ass3_Exp7
+{
+    public class Program
+    {
+        private string city;
+
+        public Program(string city_name)
+        {
+            city = city_name;
+        }
+       
+        public void SetName(string name)
+        {
+            city = name;
+        }
+
+        public string GetName()
+        {
+            return city;
+        }
+        static void Main(string[] args)
+        {
+            Program p = new Program("Mathura");
+            Console.WriteLine("Before Updation: "+p.GetName());
+            p.SetName("Delhi");
+            Console.WriteLine("After Updation: "+p.GetName());
+        }
+    }
+}
